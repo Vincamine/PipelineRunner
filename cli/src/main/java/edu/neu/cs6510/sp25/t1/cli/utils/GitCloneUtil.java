c078c8bc48736fc875832527de85aadc0f9726bf
Mingtianfang Li <li.mingtianfang@gmail.com>
2025-04-02 00:24:49 -0700

    adding util functions for checkout commit and commit within specific branch

--- cli/src/main/java/edu/neu/cs6510/sp25/t1/cli/utils/GitCloneUtil.java
+++ cli/src/main/java/edu/neu/cs6510/sp25/t1/cli/utils/GitCloneUtil.java
@@ -57,4 +57,53 @@ public class GitCloneUtil {
         .setRemoteBranchName(branch)
         .call();
   }
+
+  /**
+   * Fetches and checks out a specific commit in the given Git repo.
+   *
+   * @param repoDir the local Git repository directory
+   * @param commitHash the SHA of the commit to checkout
+   * @throws Exception if an error occurs
+   */
+  public static void checkoutCommit(File repoDir, String commitHash) throws Exception {
+    try (Git git = Git.open(repoDir)) {
+      // Fetch from origin to get all recent commits
+      git.fetch().call();
+
+      // Checkout the specific commit
+      git.checkout()
+          .setName(commitHash)
+          .call();
+    }
+  }
+
+  /**
+   * Checks out a specific commit within a given branch.
+   *
+   * This puts the repo in a detached HEAD state at the commit.
+   *
+   * @param repoDir the local Git repository directory
+   * @param branchName the name of the branch to fetch and check out (e.g., "main")
+   * @param commitHash the commit SHA to check out
+   * @throws Exception if an error occurs
+   */
+  public static void checkoutCommitInBranch(File repoDir, String branchName, String commitHash) throws Exception {
+    try (Git git = Git.open(repoDir)) {
+      // Step 1: Fetch all from origin
+      git.fetch().call();
+
+      // Step 2: Checkout the branch (create local tracking if needed)
+      git.checkout()
+          .setName(branchName)
+          .setCreateBranch(true)
+          .setStartPoint("origin/" + branchName)
+          .setUpstreamMode(org.eclipse.jgit.api.CreateBranchCommand.SetupUpstreamMode.TRACK)
+          .call();
+
+      // Step 3: Checkout the specific commit
+      git.checkout()
+          .setName(commitHash)
+          .call();
+    }
+  }
 }
