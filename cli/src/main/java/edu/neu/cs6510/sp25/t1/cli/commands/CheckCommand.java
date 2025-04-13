e555e4e101f19379864019656608fc3428c10ea8
Mingtianfang Li <li.mingtianfang@gmail.com>
2025-04-13 00:31:51 -0700

    adding check command, reused logic in backend

--- cli/src/main/java/edu/neu/cs6510/sp25/t1/cli/commands/CheckCommand.java
+++ cli/src/main/java/edu/neu/cs6510/sp25/t1/cli/commands/CheckCommand.java
@@ -1,13 +1,18 @@
 package edu.neu.cs6510.sp25.t1.cli.commands;
 
 import java.io.File;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 
+import edu.neu.cs6510.sp25.t1.common.utils.GitCloneUtil;
 import edu.neu.cs6510.sp25.t1.common.validation.error.ValidationException;
 import edu.neu.cs6510.sp25.t1.common.logging.PipelineLogger;
 import edu.neu.cs6510.sp25.t1.common.validation.validator.YamlPipelineValidator;
+import edu.neu.cs6510.sp25.t1.backend.utils.YamlPipelineUtils;
+
 import picocli.CommandLine;
+
 
 
 /**
@@ -26,12 +31,6 @@ public class CheckCommand implements Callable<Integer> {
   )
   private String filePath;
 
-  @CommandLine.Option(
-      names = {"--verbose"},
-      description = "Enable verbose output."
-  )
-  private boolean checkVerbose;
-
   /**
    * Validates a pipeline configuration file.
    *
@@ -39,42 +38,26 @@ public class CheckCommand implements Callable<Integer> {
    */
   @Override
   public Integer call() {
-    if (checkVerbose) {
-      PipelineLogger.setVerbose(true);
-    }
+
     if (filePath == null) {
       System.err.println("[Error] File path cannot be null");
       return 1;
     }
-
-    // Check if the file path is valid
-    File file = new File(filePath);
-
-    if (!file.exists()) {
-      System.err.println("[ERROR] Pipeline file " + filePath + " does not exist.");
+    if (!filePath.endsWith(".yaml")) {
+      System.err.println("[Error] File extension must be a YAML file");
       return 1;
     }
-
-    File yamlFile = new File(filePath);
-
-    if (!file.canRead()) {
-      System.err.println("[ERROR] Cannot read pipeline file: " + filePath);
+    if (!GitCloneUtil.isInsideGitRepo(new File(filePath))){
+      System.err.println("[Error] GitClone is not inside the git repo");
       return 1;
     }
-
-    if (!yamlFile.exists() || !yamlFile.isFile()) {
-      System.err.println("[Error] File not found: " + filePath);
-      return 1;
-    }
-
     try {
       System.out.println("Checking pipeline configuration: " + filePath);
 
-      if (checkVerbose) {
-        System.out.println("[DEBUG] Checking pipeline file: " + filePath);
-      }
 
-      YamlPipelineValidator.validatePipeline(filePath);
+      Map<String, Object> pipelineConfig = YamlPipelineUtils.readPipelineYaml(filePath);
+
+      YamlPipelineUtils.validatePipelineConfig(pipelineConfig);
 
       System.out.println("Pipeline configuration is valid!");
       return 0;
